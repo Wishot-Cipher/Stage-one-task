d895159c9020f8442d4434883e028bc84874c4de
Alom Wisdom <ogwisdom27@gmail.com>
2023-09-09 23:11:34 +0100

    Adjusted my UTC time functionality

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const currentDate = new Date();
-    const currentDay = daysOfWeek[currentDate.getDay()];
 
-    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDay;
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = Date.now();
+    function updateDateTime() {
+        const currentDate = new Date();
+        const currentDay = daysOfWeek[currentDate.getDay()];
+
+        document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDay;
+        document.querySelector('[data-testid="currentUTCTime"]').textContent = Date.now();
+    }
+
+    // Initial call to update the date and time
+    updateDateTime();
+
+    // Set interval updates the time every second (1000 milliseconds thats every 1seconds)
+    setInterval(updateDateTime, 1000);
 });
